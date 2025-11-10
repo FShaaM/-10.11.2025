c8b09254ea7e57ff0bcf738bea1cbc83b154a386
FShaaM <zinovevas135@gmail.com>
2025-11-10 12:57:22 +0300

    commit 5

--- main_on_p.cpp
+++ main_on_p.cpp
@@ -2,34 +2,55 @@
 
 struct IntArray
 {
+	IntArray(int i)
+	{
+
+	}
+
+	~IntArray()
+	{
+
+	}
+
 	int add(int i);
 	int get(size_t id) const;
 	size_t size() const;
+	int last() const;
 };
 
 int main()
 {
-	int next = 0;
-	std::cin >> next;
-
-	IntArray a;
-
-	a.add(next);
-
-	while (std::cin >> next)
+	try
 	{
-		a.add(next);
-	}
+		int next = 0;
+		std::cin >> next;
 
-	if (!std::cin)
+		IntArray a(next);
+
+		while (std::cin >> next)
+		{
+			a.add(next);
+		}
+
+		if (!std::cin && !std::cin.eof())
+		{
+			return 1;
+		}
+
+		size_t count = 1;
+
+		for (size_t i = 0; i < a.size() - 1; ++i)
+		{
+			int d = a.get(i);
+
+			count += !(d % a.last()) ? 1 : 0;
+		}
+
+		std::cout << count << "\n";
+	}
+	catch (...)
 	{
 		return 1;
-	}
-
-	for (size_t i = 0; i < a.size(); ++i)
-	{
-		int d = a.get(i);
-		//
 	}
 }
 
