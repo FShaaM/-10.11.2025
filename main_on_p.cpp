d064ab2bdbc59ff7a8148fa282ae2a50339cce6c
FShaaM <zinovevas135@gmail.com>
2025-11-12 12:50:17 +0300

    commit 10

--- main_on_p.cpp
+++ main_on_p.cpp
@@ -1,4 +1,4 @@
-k#include <iostream>
+#include <iostream>
 
 struct IntArray
 {
@@ -14,10 +14,20 @@ struct IntArray
 		delete[] a;
 	}
 
+	IntArray(const IntArray& rhs);
+	IntArray& operator = (const IntArray& rhs);
+
+	IntArray(IntArray&& rhs):
+		a(rhs.a),
+		k(rhs.getsize())
+	{
+		rhs.a = nullptr;
+	}
+	
 	int add(int i);
 	int get(size_t id) const noexcept;
 	/*int at(size_t id) const;*/
-	size_t size() const noexcept;
+	size_t getsize() const noexcept;
 	int last() const noexcept;
 
 	int* a;
@@ -45,7 +55,7 @@ int main()
 
 		size_t count = 1;
 
-		for (size_t i = 0; i < a.size() - 1; ++i)
+		for (size_t i = 0; i < a.getsize() - 1; ++i)
 		{
 			int d = a.get(i);
 
@@ -60,6 +70,13 @@ int main()
 	}
 }
 
+IntArray::IntArray(const IntArray& rhs) :
+	a(new int[rhs.getsize()]),
+	k(getsize())
+{
+	for (size_t i = 0; i < rhs.getsize(); ++i)
+		a[i] = rhs.get(i);
+}
 
 int IntArray::get(size_t id) const
 {
@@ -71,21 +88,21 @@ int IntArray::get(size_t id) const
 //
 //}
 
-size_t IntArray::size() const noexcept
+size_t IntArray::getsize() const noexcept
 {
 	return k;
 }
 
 int IntArray::last() const noexcept
 {
-	return get(size() - 1);
+	return get(getsize() - 1);
 }
 
 int IntArray::add(int i)
 {
-	int* dint = new int[size() + 1];
+	int* dint = new int[getsize() + 1];
 
-	for (size_t i = 0; i < size(); ++i)
+	for (size_t i = 0; i < getsize(); ++i)
 	{
 		dint[i] = get(i);
 	}
@@ -93,4 +110,19 @@ int IntArray::add(int i)
 	delete[] a;
 	a = dint;
 	++k;
+}
+
+IntArray& IntArray:: operator = (const IntArray & rhs)
+{
+	k = rhs.getsize();
+	int* p = new int[rhs.getsize()];
+
+	for (size_t i = 0; i < rhs.getsize(); ++i)
+		p[i] = rhs.get(i);
+
+	delete[] a;
+	a = p;
+	k = rhs.getsize();
+
+	return *this;
 }
